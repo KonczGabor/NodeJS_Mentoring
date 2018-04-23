b7de3b91249cc1b29b1a3d6b6a644c0234d245ce
Gabor Koncz <Gabor_Koncz@epam.com>
2018-04-23 17:38:31 +0200

    One-line import implmented.

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 import config from './config/app_config.json';
-
-/* import user from "./models/User";
-import product from "./models/Product"; */
-
-/* import * as models from "./models/*"; */
-
-import User from "./models/User";
-import Product from "./models/Product";
-
 console.log(`${config.name}`);
 
-/* console.log(User); */
+var models = require("./models");
 
-new User();
-new Product();
-
-
-
-
+for(var index in models) {
+    new models[index].default;
+}
---
+++ models/index.js
@@ -0,0 +1,2 @@
+exports.user = require("./User.js");
+exports.product = require("./Product.js");
